7e5b3baa84346d118409c01c8083ffd2117a3a72
Ankam Satya Sri Kanth <satyasrikanth.ankam@thoughtworks.com>
2022-04-08 12:41:37 +0530

    Refactor Context mechanism using react useContext hook

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context ';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+
+  const user = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(user) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {user.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {user.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {user.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        )
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {user.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {user.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {user.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
